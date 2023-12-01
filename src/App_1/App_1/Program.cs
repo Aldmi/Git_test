b960937b60c1388ce881308413f91084e0478fd3
Aldmi <Aldmi@bk.ru>
2023-12-02 00:24:15 +0700

    Update Program.cs

--- src/App_1/App_1/Program.cs
+++ src/App_1/App_1/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var ver= "1.0.0";
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
