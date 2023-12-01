c2130aca29c5cf54a76446ebd3d3cb7749bddf71
Aldmi1988 <aldmi@bk.ru>
2023-12-02 01:17:03 +0700

    Add feature_2

--- src/App_1/App_1/Program.cs
+++ src/App_1/App_1/Program.cs
@@ -6,6 +6,8 @@ var ver= "1.1.9";
 var calc= 2+1000;
 var calc2= 2+2;
 
+var feature_2 = "Super best code";
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
