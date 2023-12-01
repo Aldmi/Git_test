70829aed43aedfa2a9152a283c8b871507b09c0a
Aldmi1988 <aldmi@bk.ru>
2023-12-02 01:36:06 +0700

    remove all features

--- src/App_1/App_1/Program.cs
+++ src/App_1/App_1/Program.cs
@@ -6,14 +6,6 @@ var ver= "1.1.9";
 var calc= 2+1000;
 var calc2= 2+999;
 
-var feature_2 = "Super best code";
-
-string feature_1 = "New feature";
-
-
-string feature_100 = "New feature 100";
-
-string feature_200 = "New feature 200";
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
