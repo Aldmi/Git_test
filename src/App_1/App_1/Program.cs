ea021271fbe1fc678a600ec9514d5633c69552de
Aldmi1988 <aldmi@bk.ru>
2023-12-02 01:47:13 +0700

    remove two features

--- src/App_1/App_1/Program.cs
+++ src/App_1/App_1/Program.cs
@@ -7,9 +7,7 @@ var calc= 2+1000;
 var calc2= 2+999;
 
 
-string feature_300 = "New feature 300";
 
-string feature_1 = "New feature 1";
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
