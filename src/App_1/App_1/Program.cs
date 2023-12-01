6e8929ac6d77bb9acb1ed682d3407b802ea50d79
Aldmi1988 <aldmi@bk.ru>
2023-12-02 01:28:00 +0700

    add pullReq

--- src/App_1/App_1/Program.cs
+++ src/App_1/App_1/Program.cs
@@ -15,6 +15,8 @@ string feature_100 = "New feature 100";
 
 string feature_200 = "New feature 200";
 
+string pullReq = "New pullReq";
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
